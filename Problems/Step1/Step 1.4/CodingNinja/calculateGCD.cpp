5443c5e2d692e65e71622e4859d64a9ffc25aade
Sarvin Shrivastava <sarvin5124@gmail.com>
2023-12-15 14:39:32 +0530

    somthing

--- Problems/Step1/Step 1.4/CodingNinja/calculateGCD.cpp
+++ Problems/Step1/Step 1.4/CodingNinja/calculateGCD.cpp
@@ -2,28 +2,41 @@
 
 using namespace std;
 
-int calGCD(int a, int b) {
-      if(a != b) {
-            int min = (a < b) ? a : b;
+// int calcGCD(int a, int b) {
+//       if(a != b) {
+//             int min = (a < b) ? a : b;
 
-            cout << "Min: "<< min << "\n";
-            while(a % min != 0 && b % min != 0) {
-                  //cout << min << '\t';
-                  min--;
-            }
+//             while(min > 0) {
+//                   // cout << (a % min) << " " << (b % min)  << " " << "min: " << min << '\n';
+//                   if(a % min == 0 && b % min == 0) {
+//                         break;
+//                   }
+//                   min--;
+//             }
       
-            return min;
-      }
-      else {
-            return a;
-      }
+//             return min;
+//       }
+//       else {
+//             return a;
+//       }
+// }
+
+
+int calcGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
 int main() {
       int p, q;
 
       cin >> p >> q;
-      cout << calGCD(p, q);
+      // cout << p << " " << q << '\n';
+      cout << calcGCD(p, q);
       
       return 0;
 }
